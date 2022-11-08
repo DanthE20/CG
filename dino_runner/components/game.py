dbce179f53dc9d70005f9c8f2de3df80f84a3949
Daniel Gonzalez <crispip20@gmail.com>
2022-11-07 19:20:25 -0500

    1-Create dino component

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
+from dino_runner.components import dinosaur
 
 class Game:
     def __init__(self):
@@ -14,6 +14,8 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+
+        self.player = Dinosaur()
 
     def run(self):
         # Game loop: events - update - draw
